a6e04751bcddfd4992c8ba6382d69d3621dfdde7
Subrata Rout <subrataa916@gmail.com>
2024-01-06 10:45:06 +0530

    adding fade out project

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+let arrow = document.getElementById('arrow');
+
+window.addEventListener('scroll', () => {
+  let position = window.scrollY;
+  //console.log(position);
+  if (position <= 5) {
+    arrow.classList.toggle('fade-in');
+    arrow.classList.toggle('fade-out');
+  }
+});
